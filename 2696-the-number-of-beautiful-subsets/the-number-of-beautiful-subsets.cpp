970d0fe4c1ee9ffbea625ae52af2a005b367a3a2
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-05-23 10:13:06 +0530

    Time: 1274 ms (5.17%) | Memory: 36.7 MB (80.99%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        int ans=0;
+        bool ex[1001]={0};
+        bool b=1;
+        for(int i=0;i<(1<<nums.size());i++){
+            b=1;
+            for(int j=0;j<nums.size();j++){
+                if((i>>j)&1){
+                    ex[nums[j]]=1;
+                    if(nums[j]-k>=0 && ex[nums[j]-k]){
+                        b=0;break;
+                    }
+                    if(nums[j]+k<=1000 && ex[nums[j]+k]){
+                        b=0;break;
+                    }
+                }
+            }
+            if(b) ans++;
+            fill(ex,ex+1001,0);
+        }
+        return ans-1;
+    }
+};
