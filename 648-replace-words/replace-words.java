4b32d459b6461c750ed745bcfed704614c63fd4f
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-07 12:35:50 +0530

    Time: 97 ms (22.07%) | Memory: 56 MB (27.36%) - LeetSync

---
+++ 648-replace-words/replace-words.java
@@ -0,0 +1,49 @@
+class TrieNode{
+    TrieNode[] child;
+    boolean isEnd;
+
+    TrieNode(){
+        child= new TrieNode[26];
+        Arrays.fill(child,null);
+        isEnd=false;
+    }
+}
+
+class Solution {
+    String matchSmallest(TrieNode head,String w){
+        int matched=-1;
+        TrieNode temp=head;
+        for(int i=0;i<w.length();i++){
+            int t=(int)w.charAt(i)-(int)'a';
+            if(temp.isEnd)
+                return w.substring(0,i);
+            if(temp.child[t]==null)
+                return w;
+            temp=temp.child[t];
+        }
+        return w;
+    }
+    public String replaceWords(List<String> dict, String sentence) {
+        TrieNode head=new TrieNode();
+        TrieNode temp;
+        for(String s:dict){
+            temp=head;
+            for(int i=0;i<s.length();i++){
+                int t=(int)s.charAt(i)-(int)'a';
+                if(temp.child[t]==null)
+                    temp.child[t]=new TrieNode();
+                temp=temp.child[t];
+            }
+            temp.isEnd=true;
+        }
+        String[] words=sentence.split(" ");
+        String ans="";
+        for(int i=0;i<words.length;i++){
+            ans+= matchSmallest(head,words[i]);
+            if(i!=words.length-1)
+                ans+=" ";
+        }
+
+        return ans;
+    }
+}
