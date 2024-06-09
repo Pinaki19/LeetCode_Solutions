137b4fc1af23432cfc8317b158d38e51952d1bd3
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-09 10:49:14 +0530

    Time: 66 ms (10.83%) | Memory: 40.6 MB (5.08%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -4,17 +4,9 @@ public:
         int n=nums.size(),ans=0;
         long total=0;
         map<int,vector<int>> mp;
-        auto getRem=[&k](int n)->int{
-            int rem=n%k;
-            int canadd=0;
-            if(rem<0)
-                canadd=ceil((float)(abs(n))/k);
-            return rem<0? n+canadd*k:rem;
-        };
         for(int i=0;i<n;i++){
             total+=nums[i];
-            int rem=getRem(total);
-            //cout<<nums[i]<<" "<<rem<<' '<<total<<endl;
+            int rem=(total%k +k)%k;
             ans+=mp[rem].size();
             mp[rem].push_back(i);
         }
