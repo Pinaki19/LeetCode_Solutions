56cbc3f506fb1e7789bde1bbc219123a59bf0967
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-09 10:54:10 +0530

    Time: 772 ms (20.00%) | Memory: 29.7 MB (60.00%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -5,13 +5,14 @@ public:
         if(dp.find(cur)!=dp.end()) return dp[cur];
         if(rew[i]<=cur) return dp[cur]=getMax(i+1,cur,rew,dp);
         int iftake=cur+rew[i];
-        //int pos=lower_bound(rew.begin()+i,rew.end(),iftake)-rew.begin();
-        return dp[cur]=max(rew[i]+getMax(i+1,iftake,rew,dp),getMax(i+1,cur,rew,dp));
+        int pos=lower_bound(rew.begin()+i,rew.end(),iftake)-rew.begin();
+        return dp[cur]=max(rew[i]+getMax(pos,iftake,rew,dp),getMax(i+1,cur,rew,dp));
     }
     int maxTotalReward(vector<int>& reward) {
+        ios_base::sync_with_stdio(0);
         int n=0;
         sort(reward.begin(),reward.end());
-        map<int,int> dp;
+        unordered_map<int,int> dp;
         int ans= getMax(0,0,reward,dp);
         return ans;
     }
