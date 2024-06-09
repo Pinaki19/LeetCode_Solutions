2b01cd499c533c70503be788bb0075194bb50434
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-09 10:02:31 +0530

    Time: 1152 ms (20.00%) | Memory: 30 MB (60.00%) - LeetSync

---
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getMax(int i,int cur,auto& rew,auto& dp){
+        if(i>=rew.size()||cur>rew.back()) return 0;
+        if(dp.find(cur)!=dp.end()) return dp[cur];
+        if(rew[i]<=cur) return dp[cur]=getMax(i+1,cur,rew,dp);
+        int iftake=cur+rew[i];
+        int pos=lower_bound(rew.begin()+i,rew.end(),iftake)-rew.begin();
+        return dp[cur]=max(rew[i]+getMax(pos,iftake,rew,dp),getMax(i+1,cur,rew,dp));
+    }
+    int maxTotalReward(vector<int>& reward) {
+        int n=0;
+        sort(reward.begin(),reward.end());
+        map<int,int> dp;
+        int ans= getMax(0,0,reward,dp);
+        return ans;
+    }
+};
