1d6c28d0131ef130cd2ca46db3462a22e6a6fca7
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-13 20:08:07 +0530

    Time: 7 ms (60.18%) | Memory: 20.9 MB (52.06%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -4,9 +4,8 @@ public:
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
         int ans=0;
-        for(int i=0;i<seats.size();i++){
+        for(int i=0;i<seats.size();i++)
             ans+=abs(students[i]-seats[i]);
-        }
         return ans;
     }
 };
