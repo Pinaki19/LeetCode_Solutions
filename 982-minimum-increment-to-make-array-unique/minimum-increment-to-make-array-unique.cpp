02e1bb7e9cfaba409741b74820f8a82d14d05257
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-14 12:29:02 +0530

    Time: 73 ms (99.02%) | Memory: 69.7 MB (20.67%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
         sort(nums.begin(),nums.end());
         int ans=0,prev=-1;
         stack<int> st;
