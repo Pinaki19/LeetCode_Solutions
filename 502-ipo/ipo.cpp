d0e51ddbe0126d635480079f653dd4bc598732f9
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-15 20:02:49 +0530

    Time: 97 ms (98.58%) | Memory: 84.5 MB (74.25%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        ios_base::sync_with_stdio(0);
         priority_queue<int> pq;
         vector<pair<int,int>> vp;
         for(int i=0;i<profits.size();i++)
