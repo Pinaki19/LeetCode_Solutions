66be7538af588d0a44c117e20db9d01a8c27dc1c
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-23 10:22:03 +0530

    Time: 68 ms (70.38%) | Memory: 54.5 MB (69.11%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void show(auto q){
+        while(q.size()){
+            cout<<q.front()<<"  ";
+            q.pop_front();
+        }
+        cout<<endl;
+    }
+    int longestSubarray(vector<int>& nums, int limit) {
+        int l=0,r=0;
+        int ans=1;
+        int n=nums.size();
+        deque<int> minq,maxq;
+        while(r<n){
+            while(minq.size() && nums[r]<minq.back())
+                minq.pop_back();
+            minq.push_back(nums[r]);
+            while(maxq.size() && nums[r]>maxq.back())
+                maxq.pop_back();
+                maxq.push_back(nums[r]);
+                while(minq.size() && maxq.size() && abs(minq.front()-maxq.front())>limit){
+                    if(nums[l]==minq.front()) minq.pop_front();
+                    if(nums[l]==maxq.front()) maxq.pop_front();
+                    l++;
+                }
+                ans=max(ans,r-l+1);
+                r++;
+        }
+        return ans;
+    }
+};
