7b08b7d7db48cc24fc8d69f492ffbefb12ff4378
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-25 00:17:53 +0530

    Time: 176 ms (99.39%) | Memory: 119 MB (74.66%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.cpp
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.cpp
@@ -1,39 +1,27 @@
 class TreeAncestor {
-    int lim,n;
+    int lim;
     vector<vector<int>> ancestor;
-    vector<int> depth;
 public:
-    void get_depth(int i,auto& mp){
-        for(int x:mp[i]){
-            depth[x]=depth[i]+1;
-            get_depth(x,mp);
-        }
-    }
     TreeAncestor(int n, vector<int>& parent) {
         ios_base::sync_with_stdio(0);
-        map<int,vector<int>> mp;
-        this->n=n;
         lim=log2(n)+1;
-        ancestor=vector<vector<int>> (n,vector<int> (lim,0));
-        depth=vector<int> (n,0);
-        parent[0]=0;
-        for(int i=1;i<n;i++){
-            mp[parent[i]].push_back(i);
+        ancestor=vector<vector<int>> (n,vector<int> (lim,-1));
+        for(int i=0;i<n;i++)
             ancestor[i][0]=parent[i];
-        }
+
         for(int j=1;j<lim;j++){
-            for(int i=1;i<n;i++)
-                ancestor[i][j]=ancestor[ancestor[i][j-1]][j-1];
+            for(int i=0;i<n;i++){
+                if(ancestor[i][j-1]!=-1)
+                    ancestor[i][j]=ancestor[ancestor[i][j-1]][j-1];
+            }
         }
-        get_depth(0,mp);
-        
     }
     
     int getKthAncestor(int node, int k) {
-        if(depth[node]<k) return -1;
         for(int i=lim-1;i>=0;i--){
             if((k>>i)&1)
                 node=ancestor[node][i];
+            if(node==-1) return -1;
         }
         return node;
     }
