714bb31e6ce6e13c6ec68393d0226fb1789e08b1
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-06-26 19:35:00 +0530

    Time: 90 ms (59.57%) | Memory: 62.6 MB (40.07%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -1,29 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<int>a;
-    void inorder(TreeNode*root)
-    {
-        if(root)
-        {
-            inorder(root->left);
-            a.push_back(root->val);
-            inorder(root->right);
-        }
+    void inorder(TreeNode* root,auto& v){
+        if(!root) return;
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
     }
-
-    TreeNode* buildTree(int s, int e)
-    {
-        if(s > e) return NULL;
-        int mid = (s+e)/2;
-        TreeNode* root = new TreeNode(a[mid]);
-        root->left = buildTree(s, mid-1);
-        root->right = buildTree(mid+1, e);
+    TreeNode* balance(int l,int r,auto& v){
+        if(l>r) return nullptr;
+        int mid=l+(r-l)/2;
+        TreeNode* root=new TreeNode(v[mid]);
+        root->left=balance(l,mid-1,v);
+        root->right=balance(mid+1,r,v);
         return root;
     }
-
-    TreeNode* balanceBST(TreeNode* root) 
-    {
-        inorder(root);
-        return buildTree(0, a.size()-1);
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> nodes;
+        inorder(root,nodes);
+        return balance(0,nodes.size()-1,nodes);
     }
 };
