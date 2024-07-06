2a47f9c2911ea763931f5d80eb56e66182f2da45
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-06 15:00:48 +0530

    Time: 2 ms (54.14%) | Memory: 7.3 MB (30.58%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        n--;
+        int num_cycles=time/n;
+        int rem=time%n;
+        if(num_cycles&1){
+            return n-rem+1;
+        }else{
+            return rem+1;
+        }
+        return -1;
+    }
+};
