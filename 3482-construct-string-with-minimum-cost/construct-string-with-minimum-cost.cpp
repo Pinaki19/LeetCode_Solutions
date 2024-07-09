be4f8a2b0e6a920387e81f1903e6b88e60d79bdc
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-09 22:35:55 +0530

    Time: 1155 ms (64.58%) | Memory: 485.4 MB (77.91%) - LeetSync

--- 3482-construct-string-with-minimum-cost/construct-string-with-minimum-cost.cpp
+++ 3482-construct-string-with-minimum-cost/construct-string-with-minimum-cost.cpp
@@ -1,49 +1,100 @@
-struct Trie{
-    bool isend;
-    int cost;
-    Trie* child[26]={nullptr};
-    Trie():isend(false),cost(INT_MAX){};
+class TrieNode{
+public:
+    bool isend=false;
+    TrieNode* child[26]={nullptr};
+    int cost=(int)1e6;
+    //add anything necessary here
+
+    static TrieNode* create(auto& words,auto& costs){
+        TrieNode* head=new TrieNode();
+        TrieNode* temp=head;
+        for(int i=0;i<words.size();i++){    //index maybe useful
+            string& s=words[i];
+            temp=head;
+            for(char c:s){
+                int z=c-'a';
+                if(!temp->child[z])
+                    temp->child[z]=new TrieNode();
+                temp=temp->child[z];
+            }
+            temp->isend=true;
+            temp->cost= min(temp->cost,costs[i]);
+        }
+        return head;
+    }
+
+    void insert(std::string& word){
+        TrieNode* temp=this;
+        for (char c : word){
+            int z = c - 'a';
+            if (!temp->child[z])
+                temp->child[z] = new TrieNode();
+            temp = temp->child[z];
+        }
+        temp->isend = true;
+    }
+
+    int match_long(std::string& s,int i=0){
+        TrieNode *temp = this;
+        int ans = i - 1;
+        for (int j = i; j < s.size(); j++)
+        {
+            int z = s[j] - 'a';
+            if (!temp->child[z])
+                break;
+            if (temp->child[z]->isend)
+                ans=j;
+            temp = temp->child[z];
+        }
+        return ans + 1;
+    }
+    int match_short(std::string& s,int i=0){
+        TrieNode *temp = this;
+        int ans = i - 1;
+        for (int j = i; j < s.size(); j++){
+            int z = s[j] - 'a';
+            if (!temp->child[z]) return i;
+            if (temp->child[z]->isend)
+                return j+1;
+            temp = temp->child[z];
+        }
+        return ans + 1;
+    }
+    bool match(std::string& s,int i=0){
+        int len=s.size();
+        return this->match_long(s,i)>=len;
+    }
 };
 
 class Solution {
 public:
     int minimumCost(string target, vector<string>& words, vector<int>& costs) {
-        
-        Trie* head=new Trie;
-        vector<int> dp(target.size(),-1);
-        for(int i=0;i<words.size();i++){
-            string &s=words[i];
-            Trie* temp=head;
-            for(char c:s){
-                int z=c-'a';
-                if(!temp->child[z]){
-                    temp->child[z]=new Trie();
+        TrieNode* head=TrieNode::create(words,costs);
+        int n=target.size();
+        int dp[n];
+        fill(dp,dp+n,-1);
+
+        function<int(int)> solve=[&](int i)->int{
+            if(i>=n) return 0;
+             
+            if(dp[i]>=0) return dp[i];
+            int ans=INT_MAX;
+            TrieNode* temp=head;
+            for(int j=i;j<n;j++){
+                int z=target[j]-'a';
+                if(!temp->child[z]) break;
+                if(temp->child[z]->isend){
+                    int upto=temp->child[z]->cost;
+                    int next=solve(j+1);
+                    int total= next==INT_MAX? INT_MAX:upto+next;
+                    ans=min(ans,total);
                 }
                 temp=temp->child[z];
             }
-            temp->isend=true;
-            temp->cost=min(temp->cost,costs[i]);
-        }
-        
-       function<int(int)> solve=[&](int i)->int{
-            if(i>=target.size())return 0;
-            
-            if(dp[i]>=0) return dp[i];
-            Trie* node=head;
-            int ans=INT_MAX;
-            for(int j=i;j<target.size();j++){
-                int z=target[j]-'a';
-                if(!node->child[z]) return dp[i]=ans;
-                if(node->child[z]->isend && node->child[z]->cost<ans){ 
-                    int next=solve(j+1);
-                    int val= next==INT_MAX? INT_MAX:node->child[z]->cost+next;
-                    ans=min(ans,val);
-                }
-                node=node->child[z];
-            }
+
             return dp[i]=ans;
         };
-        for(int i=target.size()-1;i>=0;i--)
+        for(int i=n-1;i>=0;i--)
             solve(i);
         return solve(0)==INT_MAX? -1:solve(0);
     }
