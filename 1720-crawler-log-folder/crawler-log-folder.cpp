21ee5bb744385d8917051103144b1f3fd4aa5ff3
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-10 19:16:10 +0530

    Time: 43 ms (7.41%) | Memory: 31.3 MB (17.13%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -4,16 +4,10 @@ public:
         int ans=0;
         regex rgx1("[a-z0-9]+/.*$"),rgx2("\\.\\./");
         for(string& log:logs){
-            if(regex_match(log,rgx1)){
-                ans++; 
-                cout<<"rgx 1 "<<log;
-            }
-                
-            else if(regex_match(log,rgx2)){
-                cout<<"rgx 2 "<<log;
+            if(regex_match(log,rgx1))
+                ans++;    
+            else if(regex_match(log,rgx2))
                 ans=max(ans-1,0);
-            }
-                
         }
 
         return ans;
