314e86320358631396aff34ad695647fc26f6f71
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-10 19:20:31 +0530

    Time: 3 ms (86.11%) | Memory: 14 MB (61.11%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -5,10 +5,10 @@ public:
 
         for(string& log:logs){
             if(log.size()>2 && log[0]=='.'){
-                if(log[0]=='.' && log[1]=='.')
+                if(log[1]=='.')
                     ans=max(ans-1,0);
             }
-            if(log[0]!='.') ans++;
+            else if(log[0]!='.') ans++;
         }
 
         return ans;
