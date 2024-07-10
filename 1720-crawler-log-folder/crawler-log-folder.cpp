f0a0a9cde2c762ca2a6fc51f11dfbe131a3fc3c9
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-10 19:19:48 +0530

    Time: 3 ms (86.11%) | Memory: 14 MB (61.11%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int ans=0;
-        regex rgx1("[a-z0-9]+/.*$"),rgx2("\\.\\./");
+
         for(string& log:logs){
-            if(regex_match(log,rgx1))
-                ans++;    
-            else if(regex_match(log,rgx2))
-                ans=max(ans-1,0);
+            if(log.size()>2){
+                if(log[0]=='.' && log[1]=='.')
+                    ans=max(ans-1,0);
+            }
+            if(log[0]!='.') ans++;
         }
 
         return ans;
