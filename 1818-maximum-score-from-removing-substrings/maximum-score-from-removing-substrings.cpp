90143f32416a6c33c67fa7ea1d35edef326d2bf5
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-12 14:00:56 +0530

    Time: 584 ms (5.29%) | Memory: 19.4 MB (73.58%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -6,21 +6,40 @@ public:
             swap(first,second);
             swap(x,y);
         }
+        auto match=[&s](int i,string t)->bool{
+            if(i>=s.size()-1||t[0]!=s[i]) return false;
+            return s[i]==t[0] && s[i+1]==t[1];
+        };
 
         int ans=0;
-        int pos=s.find(first);
-        while(pos!=string::npos){
-            ans+=x;
-            s.erase(s.begin()+pos,s.begin()+pos+2);
-            pos=s.find(first,max(pos-1,0));
+        int i=0;
+        while(i<s.size()){
+            if(match(i,first)){
+                int l=i,r=i+1;
+                int cnt=0;
+                while(l>=0 && r<s.size() && s[l]==first[0] && s[r]==first[1])
+                    l--,r++,cnt++;
+                ans+=cnt*x;
+                s.erase(s.begin()+i-cnt+1,s.begin()+i+cnt+1);
+                i=max(0,i-cnt);
+            }else
+                i++;
         }
 
-        pos=s.find(second);
-        while(pos!=string::npos){
-            ans+=y;
-            s.erase(s.begin()+pos,s.begin()+pos+2);
-            pos=s.find(second,max(pos-1,0));
+        i=0;
+        while(i<s.size()){
+            if(match(i,second)){
+                int l=i,r=i+1;
+                int cnt=0;
+                while(l>=0 && r<s.size() && s[l]==second[0] && s[r]==second[1])
+                    l--,r++,cnt++;
+                ans+=cnt*y;
+                s.erase(s.begin()+i-cnt+1,s.begin()+i+cnt+1);
+                i=max(0,i-cnt);
+            }else
+                i++;
         }
+
         return ans;
     }
 };
