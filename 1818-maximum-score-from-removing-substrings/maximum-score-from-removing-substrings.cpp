b0bad3c1e40434b999d99bd2cdba9bef5618a6bd
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-12 14:14:03 +0530

    Time: 74 ms (72.36%) | Memory: 22.9 MB (50.81%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        string first="ab",second="ba";
+        char a='a',b='b';
         if(x<y){
-            swap(first,second);
             swap(x,y);
+            swap(a,b);
         }
-        auto match=[&s](int i,string t)->bool{
-            if(i>=s.size()-1||t[0]!=s[i]) return false;
-            return s[i]==t[0] && s[i+1]==t[1];
-        };
-
         int ans=0;
-        int i=0;
-        while(i<s.size()){
-            if(match(i,first)){
-                int l=i,r=i+1;
-                int cnt=0;
-                while(l>=0 && r<s.size() && s[l]==first[0] && s[r]==first[1])
-                    l--,r++,cnt++;
-                ans+=cnt*x;
-                s.erase(s.begin()+i-cnt+1,s.begin()+i+cnt+1);
-                i=max(0,i-cnt);
+        string temp;
+        for(char c:s){
+            if(temp.size() && temp.back()==a && c==b){
+                temp.pop_back();
+                ans+=x;
             }else
-                i++;
+                temp.push_back(c);
         }
-
-        i=0;
-        while(i<s.size()){
-            if(match(i,second)){
-                int l=i,r=i+1;
-                int cnt=0;
-                while(l>=0 && r<s.size() && s[l]==second[0] && s[r]==second[1])
-                    l--,r++,cnt++;
-                ans+=cnt*y;
-                s.erase(s.begin()+i-cnt+1,s.begin()+i+cnt+1);
-                i=max(0,i-cnt);
+        
+        s=temp;
+        temp="";
+        for(char c:s){
+            if(temp.size() && temp.back()==b && c==a){
+                temp.pop_back();
+                ans+=y;
             }else
-                i++;
+                temp.push_back(c);
         }
 
         return ans;
