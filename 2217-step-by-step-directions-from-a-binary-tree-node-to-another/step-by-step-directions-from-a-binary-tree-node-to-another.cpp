09cccf6cad1470721b963fdaffc4b5308b63e142
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-16 16:14:43 +0530

    Time: 1731 ms (8.56%) | Memory: 137.5 MB (24.01%) - LeetSync

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -13,55 +13,32 @@ class Solution {
 public:
     string getDirections(TreeNode* root, int startValue, int destValue) {
         ios_base::sync_with_stdio(0);
-        unordered_map<TreeNode*,int> depths;
-        TreeNode *u=nullptr,*v=nullptr;
-        function<TreeNode*(TreeNode*)> get_LCA=[&](TreeNode* root)->TreeNode*{
-             if (root == NULL||root->val == startValue || root->val == destValue)
-                return root;
-            TreeNode* l = get_LCA(root->left);
-            TreeNode* r = get_LCA(root->right);
-            if (l == NULL) return r;
-            if (r == NULL) return l;
-            return root;
-        };
-        auto LCA=get_LCA(root);
-
-        function<void(TreeNode*,int)> get_depths=[&](TreeNode* root,int dep)->void{
+        string to_src="",to_dst="";
+        function<void(TreeNode*)> solve=[&,&x=startValue,&y=destValue](TreeNode* root)->void{
             if(!root) return;
-            if(root->val==startValue)
-                u=root;
-            if(root->val==destValue)
-                v=root;
-            depths[root]=dep;
-            get_depths(root->left,dep+1);
-            get_depths(root->right,dep+1);
-        };
-        get_depths(root,0);
-
-        string ans="";
-        int diff=depths[u]-depths[LCA];
-        for(int i=0;i<diff;i++)
-            ans+='U';
-        if(LCA==v) return ans;
-        string left="";
-        function<bool(TreeNode*)> solve=[&](TreeNode* node)->bool{
-            if(!node) return false;
-            if(node->val==destValue)
-                return true;
-            bool to_left=solve(node->left);
-            if(to_left){
-                left+='L';
-                return true;
+            solve(root->left);
+            solve(root->right);
+            if(root->left && root->left->val==x || root->right && root->right->val==x){
+                to_src+='U';
+                x=root->val;
             }
-            bool to_right=solve(node->right);
-            if(to_right){
-                left+='R';
-                return true;
+            if(root->left && root->left->val==y){
+                to_dst+='L';
+                y=root->val;
             }
-            return false;
+            else if(root->right && root->right->val==y){
+                to_dst+='R';
+                y=root->val;
+            }
+            if(x==y){
+                x=-1;
+                y=-1;
+                return;
+            }
+                
         };
-        solve(LCA);
-        reverse(left.begin(),left.end());
-        return ans+left;
+        solve(root);
+        reverse(to_dst.begin(),to_dst.end());
+        return to_src+to_dst;
     }
 };
