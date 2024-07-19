e5ef326289b5b72dfc6a1d5b0187150687c11486
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-20 00:04:18 +0530

    Time: 60 ms (88.74%) | Memory: 48.6 MB (71.30%) - LeetSync

---
+++ 1309-sort-items-by-groups-respecting-dependencies/sort-items-by-groups-respecting-dependencies.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<int> sortItems(int n, int m, vector<int>& group, vector<vector<int>>& beforeItems) {
+
+        for(int i=0;i<n;i++)
+            if(group[i]==-1)
+                group[i]=m++;
+
+        vector<vector<int>> after_groups(m,vector<int>());
+        vector<set<int>> group_items(m,set<int>());
+        vector<int> group_indeg(m,0);
+        vector<int> indeg(n,0);
+        vector<int> ans_groups,ans;
+        vector<vector<int>> within_group(n,vector<int>());
+        for(int i=0;i<n;i++){
+            for(int item:beforeItems[i]){
+                if(group[i]!=group[item]  ){
+                    after_groups[group[item]].push_back(group[i]);
+                    group_indeg[group[i]]++;
+                }else{
+                    indeg[i]++;
+                    within_group[item].push_back(i);
+                }   
+            }
+            group_items[group[i]].insert(i);
+        }
+        // for(int i=0;i<m;i++){
+        //     cout<<i<<": "<<group_indeg[i]<<" --> ";
+        //     for(int p:group_items[i])
+        //         cout<<p<<"  ";
+        //     cout<<endl;
+        // }
+
+        queue<int> q;
+        for(int i=0;i<m;i++){
+            if(group_indeg[i]==0)
+                q.push(i);
+        }
+    
+        while(q.size()){
+            int gr_num=q.front();q.pop();
+            for(int i:after_groups[gr_num]){
+                group_indeg[i]--;
+                if(group_indeg[i]==0)
+                    q.push(i);
+            }
+            ans_groups.push_back(gr_num);
+        }
+        if(ans_groups.size()!=m) return {};
+
+        auto solve=[&](int group_num)->bool{
+            int c=0;
+            for(int i:group_items[group_num]){
+                if(indeg[i]==0)
+                    q.push(i);
+            }
+            while(q.size()){
+                int item=q.front();q.pop();
+                for(int i:within_group[item]){
+                    indeg[i]--;
+                    if(indeg[i]==0)
+                        q.push(i);
+                }
+                ans.push_back(item);
+                c++;
+            }
+            return c==group_items[group_num].size();
+        };
+        bool res;
+        for(int i:ans_groups){
+            res=solve(i);
+            if(!res) return {};
+        }
+        return ans;
+    }
+};
