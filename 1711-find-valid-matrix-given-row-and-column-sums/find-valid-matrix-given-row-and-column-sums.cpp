322e86e5da6f196173cd4e88eefb9e55e81c6562
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-20 17:10:24 +0530

    Time: 45 ms (15.80%) | Memory: 36.2 MB (11.51%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -2,29 +2,18 @@ class Solution {
 public:
     int m,n;
     void solve(int x,int y,auto& rowSum,auto& colSum,auto& ans,bool& solved){
-         if(x==m){
-                for(int i=0;i<m;i++)
-                    if(rowSum[i])
-                        return;
-                for(int j=0;j<n;j++)
-                    if(colSum[j]) 
-                        return;
-                solved=true;
-                return;
-            }
-            if(solved) return;
-            int nx= y==n-1? x+1:x;
-            int ny= y==n-1? 0:y+1;
-
-            for(int i=min(rowSum[x],colSum[y]);i>=0;i--){
-                rowSum[x]-=i;
-                colSum[y]-=i;
-                ans[x][y]=i;
-                solve(nx,ny,rowSum,colSum,ans,solved);
-                if(solved) return;
-                rowSum[x]+=i;
-                colSum[y]+=i;
-            }
+        if(x==m){
+            solved=true;
+            return;
+        }
+        if(solved) return;
+        int nx= y==n-1? x+1:x;
+        int ny= y==n-1? 0:y+1;
+        int i=min(rowSum[x],colSum[y]);
+        rowSum[x]-=i;
+        colSum[y]-=i;
+        ans[x][y]=i;
+        solve(nx,ny,rowSum,colSum,ans,solved);
     }
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
         m=rowSum.size();
