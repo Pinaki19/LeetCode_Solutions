18fc0ed678067476886dae8ade3a5ea911985a76
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-20 17:11:35 +0530

    Time: 42 ms (28.89%) | Memory: 36.1 MB (11.51%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int m,n;
-    void solve(int x,int y,auto& rowSum,auto& colSum,auto& ans,bool& solved){
-        if(x==m){
-            solved=true;
+    void solve(int x,int y,auto& rowSum,auto& colSum,auto& ans){
+        if(x==m)
             return;
-        }
-        if(solved) return;
         int nx= y==n-1? x+1:x;
         int ny= y==n-1? 0:y+1;
         int i=min(rowSum[x],colSum[y]);
         rowSum[x]-=i;
         colSum[y]-=i;
         ans[x][y]=i;
-        solve(nx,ny,rowSum,colSum,ans,solved);
+        solve(nx,ny,rowSum,colSum,ans);
     }
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
         m=rowSum.size();
         n=colSum.size();
-        bool solved=false;
         vector<vector<int>> ans(m,vector<int>(n,0));
 
-        solve(0,0,rowSum,colSum,ans,solved);
+        solve(0,0,rowSum,colSum,ans);
         return ans;
     }
 };
