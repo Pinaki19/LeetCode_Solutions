3e80e7c11b2ce4adbb8fbf2879504822140c6421
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-21 10:36:21 +0530

    Time: 105 ms (100.00%) | Memory: 124.5 MB (100.00%) - LeetSync

--- 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
@@ -6,13 +6,12 @@ public:
         int n = nums.size();
         vector<int> diffs(n);
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) 
             diffs[i] = nums[i] - target[i];
-        }
+        
         ll ans=0;
         ll prev=0;
         for(int i:diffs){
-            cout<<i<<" ";
             if(i<0){
                 if(i<prev)
                     ans+=-i+min(0ll,prev);
