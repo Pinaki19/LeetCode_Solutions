304d88e9ecd047499c56073ad327e0a5f622af17
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-24 16:29:49 +0530

    Time: 156 ms (87.88%) | Memory: 88.8 MB (55.84%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        sort(nums.begin(),nums.end(),[&mapping](int i,int j){
-            if(i==j) return false;
-            string is=to_string(i);
-            string js=to_string(j);
-            int p=0,q=0;
-            while(p<is.size() && mapping[is[p]-'0']==0)
-                p++;
-            while(q<js.size() && mapping[js[q]-'0']==0)
-                q++;
-            //cout<<is<<"  "<<js<<endl;
-            if(is.size()-p<js.size()-q) return true;
-            if(js.size()-q<is.size()-p) return false;
-            for(;p<is.size() && q<js.size() ;p++,q++){
-                int k1=is[p]-'0',k2=js[q]-'0';
-                if(mapping[k1]==mapping[k2]) continue;
-                if(mapping[k1]<mapping[k2])
-                    return true;
-                else
-                    return false;
+        vector<pair<int,int>> nmap;
+        auto convert=[&mapping](int i)->int{
+            string s=to_string(i);
+            int num=0;
+            for(char c:s){
+                num=num*10+mapping[c-'0'];
             }
-            return false;
-        });
-        return nums;
+            return num;
+        };
+
+        for(int i=0;i<nums.size();i++)
+            nmap.push_back({convert(nums[i]),i});
+        sort(nmap.begin(),nmap.end());
+
+        vector<int> ans;
+        for(auto [_,ind]:nmap){
+            ans.push_back(nums[ind]);
+        }
+           
+        return ans;
     }
 };
