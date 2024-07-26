d94a3de3532c85bc8bcd9b8acd45ebe706325b8d
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-07-27 01:40:31 +0530

    Time: 292 ms (47.78%) | Memory: 132.5 MB (60.92%) - LeetSync

---
+++ 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
@@ -0,0 +1,23 @@
+using ll=long long;
+class Solution {
+public:
+    long long numberOfSubarrays(vector<int>& nums) {
+        int l=0,r=0;
+        int n=nums.size();
+        stack<int> st;
+        map<int,int> mp;
+        ll ans=0;
+        while(r<n){
+            ans++;
+            while(st.size() && nums[r]>st.top()){
+                mp[st.top()]=0;
+                st.pop();
+            } 
+            st.push(nums[r]);
+            ans+=mp[st.top()];
+            mp[nums[r]]++;
+            r++;
+        }
+        return ans;
+    }
+};
