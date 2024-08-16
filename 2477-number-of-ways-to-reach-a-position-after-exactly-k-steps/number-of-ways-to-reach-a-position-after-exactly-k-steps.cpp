c7150280a79287fcca07af9b5de04d3ec02b1e57
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-08-16 12:40:57 +0530

    Time: 943 ms (12.98%) | Memory: 105.4 MB (61.96%) - LeetSync

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -4,7 +4,7 @@ public:
     int solve(int i,int k,int end,auto& dp){
         if(k<0) return 0;
         int diff=abs(end-i);
-        if(diff>k) return 0;
+        if(diff>k||((k-diff)&1)) return 0;
         if(dp.find({i,k})!=dp.end())
             return dp[{i,k}];
         long ans=i==end && k==0;
