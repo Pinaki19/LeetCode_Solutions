37b1e1c30f69c3635041cd52a1b2c860b616601c
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-08-16 12:40:30 +0530

    Time: 1212 ms (11.39%) | Memory: 135.7 MB (46.70%) - LeetSync

---
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    const int MOD=(int)1e9+7;
+    int solve(int i,int k,int end,auto& dp){
+        if(k<0) return 0;
+        int diff=abs(end-i);
+        if(diff>k) return 0;
+        if(dp.find({i,k})!=dp.end())
+            return dp[{i,k}];
+        long ans=i==end && k==0;
+        ans+=solve(i+1,k-1,end,dp);
+        ans%=MOD;
+        ans+=solve(i-1,k-1,end,dp);
+        ans%=MOD;
+        return dp[{i,k}]=ans;
+    }
+    int numberOfWays(int startPos, int endPos, int k) {
+        map<array<int,2>,int> dp;
+        return solve(startPos,k,endPos,dp);
+    }
+};
