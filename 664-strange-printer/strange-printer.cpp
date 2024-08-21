e24d55c4ed5930bb2a51324ec9850249d657fa8c
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-08-21 11:27:17 +0530

    Time: 6 ms (98.46%) | Memory: 8.8 MB (84.33%) - LeetSync

---
+++ 664-strange-printer/strange-printer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dp[101][101];
+    int solve(auto& s,int l,int r){
+        if(l>r) return 0;
+        if(l==r) return dp[l][r]=1;
+        if(dp[l][r]!=-1) return dp[l][r];
+        if(s[l]==s[r])
+            return dp[l][r]=solve(s,l+1,r);
+ 
+        int ans=1+solve(s,l+1,r);
+        for(int j=l+1;j<=r;j++){
+            if(s[j]==s[l])
+                ans=min(ans,solve(s,l+1,j)+solve(s,j+1,r));
+        }
+        return dp[l][r]=ans;
+    }
+    int strangePrinter(string s) {
+        memset(dp,-1,sizeof(dp));
+        string temp="";
+        int i=0;
+        while(i<s.size()){
+            char c=s[i];
+            while(i<s.size() && s[i]==c)
+                i++;
+            temp+=c;
+        }
+        return solve(temp,0,temp.size()-1);
+    }
+};
