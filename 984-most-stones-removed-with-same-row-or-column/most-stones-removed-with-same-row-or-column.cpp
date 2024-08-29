17af281f3d97a87775d0f67346a35c4c3c5cd3ed
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-08-29 21:44:01 +0530

    Time: 32 ms (69.93%) | Memory: 25.3 MB (20.21%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int ans=0;
+        int n=stones.size();
+        const int lim=1e3+1;
+        bitset<lim> bs;
+        bs.reset();
+
+        unordered_map<int,vector<int>> ri,ci;
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<n;i++){
+            int rv=stones[i][0],cv=stones[i][1];
+            ri[rv].push_back(i);
+            ci[cv].push_back(i);
+        }
+
+        auto bfs=[&](int x,int y)->int{
+            int num=0;
+            q.push({x,y});
+            while(q.size()){
+                auto [rv,cv]=q.front();
+                num++;
+                q.pop();
+                for(int i:ri[rv]){
+                    if(!bs[i])
+                        bs[i]=1,q.push({stones[i][0],stones[i][1]});
+                }
+                for(int i:ci[cv]){
+                    if(!bs[i])
+                        bs[i]=1,q.push({stones[i][0],stones[i][1]});
+                }
+            }
+            return num-1;
+        };
+
+        for(int i=0;i<n;i++){
+            if(bs[i]==0){
+                bs[i]=1;
+                ans+= bfs(stones[i][0],stones[i][1]);
+            }
+        }
+
+        return ans;
+    }
+};
