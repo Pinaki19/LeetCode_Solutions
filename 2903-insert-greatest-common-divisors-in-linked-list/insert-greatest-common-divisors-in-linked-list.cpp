6a9e580512b47cd346d38654ad32ce42792d2111
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-09-10 10:11:24 +0530

    Time: 52 ms (19.96%) | Memory: 35.5 MB (49.64%) - LeetSync

---
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* temp=head;
+        int g;
+        while(temp->next){
+            g=gcd(temp->val,temp->next->val);
+            temp->next=new ListNode(g,temp->next);
+            temp=temp->next;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
