0f6564fb8024854043aa65d4aeb7847e5e78f70e
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-09-14 22:13:41 +0530

    Time: 104 ms (62.28%) | Memory: 84.8 MB (77.85%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -4,16 +4,13 @@ public:
         int n=nums.size();
         int ans=1;
         int maxVal=*max_element(nums.begin(),nums.end());
-        vector<bool> visited(n,false);
         for(int i=0;i<n;i++){
-            if(visited[i]||nums[i]!=maxVal) continue;
+            if(nums[i]!=maxVal) continue;
             int r=i+1;
-            visited[i]=true;
-            int curAnd=nums[i];
-            while(r<n && nums[r]==nums[i]){
-                visited[r++]=true;
-            }
+            while(r<n && nums[r]==nums[i])
+                r++;
             ans=max(ans,r-i);
+            i=r;
         }
 
         return ans;
