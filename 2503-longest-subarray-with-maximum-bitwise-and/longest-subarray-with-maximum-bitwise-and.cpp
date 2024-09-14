fc4ab63854fab6eb47605925ae466d8771d5761d
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-09-14 22:12:41 +0530

    Time: 106 ms (55.70%) | Memory: 85.4 MB (13.82%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -3,50 +3,17 @@ public:
     int longestSubarray(vector<int>& nums) {
         int n=nums.size();
         int ans=1;
-        int cur=0;
         int maxVal=*max_element(nums.begin(),nums.end());
-        vector<int> temp;
-        for(int i=0;i<n;i++){
-            if(nums[i]==maxVal)
-                temp.push_back(i);
-        }
         vector<bool> visited(n,false);
-        for(int i:temp){
-            if(visited[i]) continue;
-            int l=i,r=i;
+        for(int i=0;i<n;i++){
+            if(visited[i]||nums[i]!=maxVal) continue;
+            int r=i+1;
             visited[i]=true;
             int curAnd=nums[i];
-            while(l>0){
-                int left=nums[l-1];
-                bool allSet=true;
-                for(int i=0;i<32;i++){
-                    if((curAnd>>i)&1){
-                        if(((left>>i)&1)==0){
-                            allSet=false;
-                            break;
-                        }
-                    }
-                }
-                if(!allSet) break;
-                visited[l]=true;
-                l--;
+            while(r<n && nums[r]==nums[i]){
+                visited[r++]=true;
             }
-            while(r<n-1){
-                int right=nums[r+1];
-                bool allSet=true;
-                for(int i=0;i<32;i++){
-                    if((curAnd>>i)&1){
-                        if(((right>>i)&1)==0){
-                            allSet=false;
-                            break;
-                        }
-                    }
-                }
-                if(!allSet) break;
-                visited[r]=true;
-                r++;
-            }
-            ans=max(ans,r-l+1);
+            ans=max(ans,r-i);
         }
 
         return ans;
