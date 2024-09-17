cb119c1d18ca44b8a12ea122bf8e53995c6b8bb8
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2024-09-17 21:49:58 +0530

    Time: 7 ms (31.51%) | Memory: 16.8 MB (76.07%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int find_pivot(auto& nums){
+        int l=0,r=nums.size()-1;
+        int ans=r;
+        while(l<r){
+            if(nums[l]<nums[r])
+                return r;
+            if(r-l+1==2){
+                return nums[l]>nums[r]? l:r;
+            }
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[l])
+                l=mid;
+            else if(nums[mid]<nums[l])
+                r=mid-1;
+            else{
+              while(l<r){
+                if(nums[l]>nums[l+1]) return l;
+                l++;
+              }
+            }
+        }
+        return l;
+    }
+    bool search(vector<int>& nums, int target) {
+        int pivot=find_pivot(nums);
+        int l=0,r=pivot;
+        if(target<nums[l])
+            l=pivot+1,r=nums.size()-1;
+        cout<<pivot<<endl;
+        
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target) return true;
+            if(l==r) break;
+            if(nums[mid]>target)
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return false;
+    }
+};
