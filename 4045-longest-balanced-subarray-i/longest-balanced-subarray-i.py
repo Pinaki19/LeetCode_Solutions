52ebd4034f1c3c6d9f0a0eae6c9cc96e64ba4c2b
PINAKI BANERJEE <96727999+Pinaki19@users.noreply.github.com>
2026-02-10 21:12:30 +0530

    Time: 1274 ms (62.98%) | Memory: 19.5 MB (57.02%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
@@ -2,16 +2,17 @@ class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
         n=len(nums)
         ans=0
+        evens=set()
+        odds=set()
         for i in range(n):
-            evens=defaultdict(int)
-            odds=defaultdict(int)
+            
             for j in range(i,n):
                 if((nums[j]&1)==1):
-                    odds[nums[j]]+=1
+                    odds.add(nums[j])
                 else:
-                    evens[nums[j]]+=1
-
+                    evens.add(nums[j])
                 if len(evens)==len(odds):
                     ans=max(ans,j-i+1)
-            
+            odds.clear()
+            evens.clear()
         return ans
